44b435f9d3811e66c53eaa62353a4f3c7219d995
Diego Souza <di.b@hotmail.com>
2024-02-20 16:37:34 -0300

    ajuste 200224

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -1,1 +1,8 @@
 //efeito de esconder formulário de cadastro
+$(document).ready(function(){
+    $("#botao-adastrar").click(function(){
+        $("#form-cadastrar").slideToggle("slow");
+        $("#section-login").slideToggle("slow");
+        $("#botao-cadastrar").hide();
+    });
+});
